3f64591f755b6721a068e63c45c7d466625295b5
amarildo <amarildo.rajta@gmail.com>
2024-09-16 19:21:56 +0200

    make webhook input box a daisyui password input component, and add a key icon label for it

--- src/lib/components/SettingsView.svelte
+++ src/lib/components/SettingsView.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import {createEventDispatcher} from 'svelte';
+    import { createEventDispatcher } from "svelte";
+    import KeyRound from "$assets/icons/KeyRound.svelte";
 
     export let webhookUrl: string;
 
@@ -7,25 +8,21 @@
     let inputUrl = webhookUrl;
 
     function saveSettings() {
-        dispatch('update', inputUrl);
+        dispatch("update", inputUrl);
     }
 </script>
 
 <div class="flex flex-col items-center gap-2.5">
     <h2>Settings</h2>
     <label for="webhook-url">Discord Webhook URL:</label>
-    <input
+    <label class="input input-bordered flex items-center gap-2">
+        <KeyRound />
+        <input
             id="webhook-url"
-            type="text"
+            type="password"
             bind:value={inputUrl}
             placeholder="Enter Discord webhook URL"
-    />
+        />
+    </label>
     <button class="btn" on:click={saveSettings}>Save</button>
 </div>
-
-<style>
-    input {
-        width: 300px;
-        padding: 5px;
-    }
-</style>
