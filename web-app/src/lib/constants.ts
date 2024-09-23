9867ded9c58ff50ebffd91a4caf41b6f1d91008a
amarildo <amarildo.rajta@gmail.com>
2024-09-23 20:58:54 +0200

    add types for constants and add gh constants

--- web-app/src/lib/constants.ts
+++ web-app/src/lib/constants.ts
@@ -1,3 +1,10 @@
-export const ENCRYPTION_ALGORITHM = "aes-256-cbc";
-export const DISCORD_API_ENDPOINT = "/api/webhook";
-export const LOCAL_STORAGE_DISCORD_KEY = "discordWebhookUrl";
+export const ENCRYPTION_ALGORITHM: string = "aes-256-cbc";
+export const DISCORD_API_ENDPOINT: string = "/api/webhook";
+export const LOCAL_STORAGE_DISCORD_KEY: string = "discordWebhookUrl";
+
+export const PROJECT_TITLE: string = "Disco Beacon";
+
+export const GITHUB = {
+  USERNAME: "bl4ckswordsman",
+  REPO_NAME: "disco-beacon",
+} as const;
