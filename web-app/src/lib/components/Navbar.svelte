475039faa4b6c66b081f5c2a342ac25f37b35f28
amarildo <amarildo.rajta@gmail.com>
2024-09-23 21:09:19 +0200

    add daisyui navbar component

---
+++ web-app/src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import GitHub from "$assets/icons/GitHub.svelte";
+
+    export let githubLink: string;
+</script>
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a href="/" class="btn btn-ghost text-xl">Disco Beacon</a>
+    </div>
+    <div class="flex-none">
+        <a
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            class="btn btn-square btn-ghost"
+        >
+            <GitHub />
+        </a>
+    </div>
+</div>
