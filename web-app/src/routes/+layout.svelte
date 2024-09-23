c27bb3e0c4e34488f9191a90db43d807720ce1b6
amarildo <amarildo.rajta@gmail.com>
2024-09-23 20:29:54 +0200

    add footer that is sticky but doesn't overlap content

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -2,4 +2,13 @@
     import "../app.css";
 </script>
 
-<slot/>
+<div class="flex flex-col min-h-screen">
+    <main class="flex-grow">
+        <slot />
+    </main>
+    <footer class="footer footer-center p-4 mt-4">
+        <div class="container mx-auto text-center">
+            © {new Date().getFullYear()} bl4ckswordsman
+        </div>
+    </footer>
+</div>
