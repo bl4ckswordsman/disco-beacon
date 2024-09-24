01ad563defc2bda0982b23b190290de7958fd6dc
amarildo <amarildo.rajta@gmail.com>
2024-09-24 12:40:25 +0200

    fix toast showing behind dialog/modal by giving it a z value of 9999

--- web-app/src/lib/components/Toast.svelte
+++ web-app/src/lib/components/Toast.svelte
@@ -3,7 +3,7 @@
     export let type: "info" | "success" | "error" = "info";
 </script>
 
-<div class="toast toast-bottom toast-center">
+<div class="toast toast-bottom toast-center z-[9999]">
     <div class="alert alert-{type}">
         <span>{message}</span>
     </div>
