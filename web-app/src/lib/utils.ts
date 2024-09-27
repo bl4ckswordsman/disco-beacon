c3e03ff9d8372cc789ce8aeb4db3481edb6362a3
amarildo <amarildo.rajta@gmail.com>
2024-09-27 12:44:29 +0200

    add codacy fix manually

--- web-app/src/lib/utils.ts
+++ web-app/src/lib/utils.ts
@@ -1,14 +1,14 @@
-import {LOCAL_STORAGE_DISCORD_KEY} from "$lib/constants";
+import { LOCAL_STORAGE_DISCORD_KEY } from "$lib/constants";
 
 export function getStoredWebhookUrl(): string {
-    if (typeof localStorage !== 'undefined') {
-        return localStorage.getItem(LOCAL_STORAGE_DISCORD_KEY) || "";
-    }
-    return "";
+  if (typeof localStorage !== "undefined") {
+    return localStorage.getItem(LOCAL_STORAGE_DISCORD_KEY) ?? "";
+  }
+  return "";
 }
 
 export function setStoredWebhookUrl(url: string): void {
-    if (typeof localStorage !== 'undefined') {
-        localStorage.setItem(LOCAL_STORAGE_DISCORD_KEY, url);
-    }
+  if (typeof localStorage !== "undefined") {
+    localStorage.setItem(LOCAL_STORAGE_DISCORD_KEY, url);
+  }
 }
