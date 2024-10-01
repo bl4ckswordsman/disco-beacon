e719d46325a9ec42a31523b882538bfe18af0a81
amarildo <amarildo.rajta@gmail.com>
2024-10-01 19:30:24 +0200

    fix: Update both the constants.ts and crypto.ts files to ensure proper typing of the ENCRYPTION_ALGORITHM

--- web-app/src/lib/constants.ts
+++ web-app/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const ENCRYPTION_ALGORITHM = "aes-256-cbc";
+export const ENCRYPTION_ALGORITHM: string = "aes-256-cbc";
 export const DISCORD_API_ENDPOINT = "/api/webhook";
 export const LOCAL_STORAGE_DISCORD_KEY = "discordWebhookUrl";
 
--- web-app/src/lib/server/crypto.ts
+++ web-app/src/lib/server/crypto.ts
@@ -1,15 +1,17 @@
 import * as crypto from "crypto";
 import { ENCRYPTION_ALGORITHM } from "$lib/constants";
 
-const algorithm: string = ENCRYPTION_ALGORITHM;
-
 function hexToBuffer(hex: string): Buffer {
   return Buffer.from(hex, "hex");
 }
 
 export function encrypt(text: string, key: string): string {
   const iv: Buffer = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, hexToBuffer(key), iv);
+  const cipher = crypto.createCipheriv(
+    ENCRYPTION_ALGORITHM,
+    hexToBuffer(key),
+    iv,
+  );
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return iv.toString("hex") + ":" + encrypted;
@@ -19,7 +21,11 @@ export function decrypt(text: string, key: string): string {
   const textParts: string[] = text.split(":");
   const iv: Buffer = Buffer.from(textParts.shift() ?? "", "hex");
   const encryptedText: string = textParts.join(":");
-  const decipher = crypto.createDecipheriv(algorithm, hexToBuffer(key), iv);
+  const decipher = crypto.createDecipheriv(
+    ENCRYPTION_ALGORITHM,
+    hexToBuffer(key),
+    iv,
+  );
   let decrypted = decipher.update(encryptedText, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
