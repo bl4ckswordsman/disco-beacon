58e3c9c35ce43f3d94e6a4c48b27e58bfa743f08
amarildo <amarildo.rajta@gmail.com>
2024-10-05 17:55:46 +0200

    fix attempt 2

--- web-app/src/lib/stores/payload-store.ts
+++ web-app/src/lib/stores/payload-store.ts
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 import type { WebhookPayload } from "$lib/types";
 
-const defaultPayload: WebhookPayload = {
+const defaultPayload = {
   content: "The Valheim server is up. @everyone",
   embeds: [
     {
@@ -10,6 +10,9 @@ const defaultPayload: WebhookPayload = {
       color: 5763719,
     },
   ],
-};
+} as const;
 
-export const webhookPayload = writable<WebhookPayload>(defaultPayload);
+export const webhookPayload = writable<WebhookPayload>({
+  ...defaultPayload,
+  embeds: [...defaultPayload.embeds],
+});
