0148a266903467605ebf56091f4ddf9efe52bd55
amarildo <amarildo.rajta@gmail.com>
2024-10-05 18:44:37 +0200

    Update the `settings-store.ts` file to properly type the `webhookUrl` store

--- web-app/src/lib/stores/settings-store.ts
+++ web-app/src/lib/stores/settings-store.ts
@@ -1,8 +1,10 @@
-import { writable } from "svelte/store";
+import { writable, type Writable } from "svelte/store";
 import { getStoredWebhookUrl, setStoredWebhookUrl } from "$lib/utils";
-export const webhookUrl = writable<string>(getStoredWebhookUrl() as string);
+export const webhookUrl: Writable<string> = writable<string>(
+  getStoredWebhookUrl() as string,
+);
 
-webhookUrl.subscribe((value) => {
+webhookUrl.subscribe((value: string) => {
   try {
     setStoredWebhookUrl(value);
   } catch (error) {
