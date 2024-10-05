c4b80d7886564acc0af948ec4292ba523dbff9f7
amarildo <amarildo.rajta@gmail.com>
2024-10-05 18:32:10 +0200

    Update the `settings-store.ts` file to ensure type safety for the `subscribe` method

--- web-app/src/lib/stores/settings-store.ts
+++ web-app/src/lib/stores/settings-store.ts
@@ -2,7 +2,9 @@ import { writable } from "svelte/store";
 import { getStoredWebhookUrl, setStoredWebhookUrl } from "$lib/utils";
 export const webhookUrl = writable<string>(getStoredWebhookUrl() as string);
 
-webhookUrl.subscribe((value) => {
+(
+  webhookUrl as { subscribe: (callback: (value: string) => void) => void }
+).subscribe((value) => {
   try {
     setStoredWebhookUrl(value);
   } catch (error) {
